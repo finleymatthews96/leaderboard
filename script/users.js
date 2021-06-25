1bacd28ec1ed7efefe36574018bff25eef84e5e4
Finley Matthews <finleymatthews96@gmail.com>
2021-06-25 15:53:34 -0400

    seed users with scores in redis

---
+++ script/users.js
@@ -0,0 +1,44 @@
+const users = [
+  {
+    username: "cody",
+    password: "123",
+    score: 5,
+  },
+  {
+    username: "john",
+    password: "123",
+    score: 6,
+  },
+  {
+    username: "peter",
+    password: "123",
+    score: 7,
+  },
+  {
+    username: "paul",
+    password: "123",
+    score: 50,
+  },
+  {
+    username: "mary",
+    password: "123",
+    score: 9,
+  },
+  {
+    username: "vanessa",
+    password: "123",
+    score: 10,
+  },
+  {
+    username: "yoyo",
+    password: "123",
+    score: 11,
+  },
+  {
+    username: "aaron",
+    password: "123",
+    score: 12,
+  },
+];
+
+module.exports = users;
